3bacf43550f53d68d98d3000c7be746ef70b4609
peak2548 <peakpairch@gmail.com>
2025-03-03 15:10:33 +0700

    Draft

--- GameRunner.java
+++ GameRunner.java
@@ -2,6 +2,8 @@ import javax.swing.JFrame;
 import java.awt.Color;
 
 public class GameRunner extends JFrame {
+    private Screen scr;
+
     public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
 
@@ -13,6 +15,8 @@ public class GameRunner extends JFrame {
         getContentPane().setBackground(Color.BLACK);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setFocusable(true);
+        scr = new Screen();
+        add(scr);
         setVisible(true);
     }
     public static void main(String[] args) {
---
+++ Screen.java
@@ -0,0 +1,8 @@
+import javax.swing.JPanel;
+
+public class Screen extends JPanel implements Runnable {
+
+    public Screen() {
+        
+    }
+}
